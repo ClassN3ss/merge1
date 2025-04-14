25d22b27ee865f859f5453ae4a3dc138f6368188
PathaninThima <nattawut.thima@gmail.com>
2025-04-14 22:35:29 +0700

    delet2

--- Face-backend/routes/enrollRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getEnrolledSubjects } = require("../controllers/enrollController");
-const { verifyToken } = require("../middleware/authMiddleware");
-
-router.get("/enrolled/:studentId", verifyToken, getEnrolledSubjects);
-
-module.exports = router;
